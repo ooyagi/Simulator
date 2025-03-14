01d82a7c0e8d36ae9708ba0bdcf7072daa4d296b
ooyagi <t_oyagi@outlook.jp>
2025-03-14 09:16:00 +0900

    [feat]IP: 在庫パレットの搬入時には毎回ロケーションを作成する

--- InventoryPalletManager/services/InboundInventoryPalletServices.cs
+++ InventoryPalletManager/services/InboundInventoryPalletServices.cs
@@ -26,17 +26,16 @@ class InboundInventoryPalletServices: IInboundInventoryPalletServices
 
     /// <summary>
     /// 在庫パレット入庫サービス
-    /// 
-    /// 搬送数試算のためロケーションは無制限としている
-    /// 空きロケーションが見つからない場合は新規ロケーションを作成する
     /// </summary>
     /// <remarks>
+    /// 搬送数試算のためロケーションは無制限としているため搬入毎に新規ロケーションを作成している
+    /// 
     /// 現在は在庫パレット置き場に直接新規パレットを置く形になっているが
     /// 本番システムでは搬入口に置かれたタイミングで在庫パレットを登録するので状況が異なる
     /// </remarks>
     public InventoryPallet Inbound(Hinban hinban) {
         var pallets = new InventoryPallet(hinban);
-        var emptyStorage = _inventoryStorageLoader.FindEmptyLocation() ?? _inventoryStorageManagementService.AddAuto();
+        var emptyStorage = _inventoryStorageManagementService.AddAuto();
         emptyStorage.Place(pallets.Id);
         _context.InventoryPallets.Add(pallets);
         _context.SaveChanges();

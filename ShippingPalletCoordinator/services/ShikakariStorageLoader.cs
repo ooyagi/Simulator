03aff7406f1794097cee0a57e2c2bf8ab6715117
ooyagi <t_oyagi@outlook.jp>
2025-03-24 17:48:20 +0900

    [feat]SP: 基本的に状態が InUse であればパレット情報は null では無いが念の為ガードする
    EntityFrameworkのキャッシュなのか、DBのリレーションの更新タイミングなのか
    稀に null が入ることがあるため。

--- ShippingPalletCoordinator/services/ShikakariStorageLoader.cs
+++ ShippingPalletCoordinator/services/ShikakariStorageLoader.cs
@@ -35,7 +35,7 @@ class ShikakariStorageLoader: Services.IShikakariStorageLoader, ShippingOperatio
         var shikakariStorages = _context.ShikakariStorages
             .Include(x => x.StoredPallet)
             .ThenInclude(x => x.Items)
-            .Where(x => x.Status == StorageStatus.InUse)
+            .Where(x => x.Status == StorageStatus.InUse && x.StoredPallet != null)
             .ToList();
         var loadableItems = loadablePallets.Select(x => new LoadableItem(x.Hinban, x.Quantity)).ToList();
         return shikakariStorages
